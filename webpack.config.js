84f0e875d296d6f16225e447b7879f5744ff33c6
Gustav Tonér <gustav.toner@gmail.com>
2016-04-24 20:07:35 +0200

    Update dist filename to simplify dev

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ var webpack = require("webpack");
 module.exports = {
   entry: './app/index.jsx',
   output: {
-    path: './dist/',
+    path: '.',
     filename: 'app.dist.js',
   },
   devtool: '#source-map',
