6fb533ddf7676d7eb080b83ca9570590095545d2
crilleconsid <christopher.gustafsson@consid.se>
2016-09-21 12:39:39 +0200

    CSS update

--- app/SearchBar.jsx
+++ app/SearchBar.jsx
@@ -8,8 +8,11 @@ var SearchBar = React.createClass({
     },
   render: function() {
     return (
-      <div className="ms-SearchBox">
-        <input className="ms-SearchBox-field" ref="searchInput" value={this.props.query} onChange={this.doSearch} type="text" placeholder="Search..." />
+        <div className="ms-SearchBox center ms-Grid-col ms-u-sm12">
+            <div class="searchbar">
+                <i class="fa fa-search" aria-hidden="true"></i>
+                <input className="ms-SearchBox-field" ref="searchInput" value={this.props.query} onChange={this.doSearch} type="text" placeholder="Search..." />
+            </div>
         </div>
     );
   }
